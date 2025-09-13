4ae609a2061a2981aab41d06dfc3453ccc54d784
SWASTIK BANERJEE <swastikbanerjee@Mac.lan>
2025-09-13 23:26:00 +0530

    changes

--- stroutputparser.py
+++ stroutputparser.py
@@ -6,7 +6,7 @@ from langchain_core.prompts import PromptTemplate
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    repo_id="google/gemma-2-2b-it",
     task="text-generation"
 )
 model = ChatHuggingFace(llm=llm)
